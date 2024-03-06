0a44865af33ba7c888cc7a110d710c76b8cca776
MV <MV@MACBEK>
2024-03-06 10:11:28 +0200

    Добавьте файлы проекта.

---
+++ Lab/Program.cs
@@ -0,0 +1,19 @@
+﻿List<int> listint = new List<int> { -3, -6, 7, 12, -2 };
+int i = 0, j = listint.Count - 1;
+while (i < j)
+{
+    while (i < j && listint[i] > 0) i++;
+    while (i < j && listint[j] < 0) j--;
+    if (i < j)
+    {
+        int temp = listint[i];
+        listint[i] = listint[j];
+        listint[j] = temp;
+        i++;
+        j--;
+    }
+}
+foreach (int item in listint)
+{
+    Console.Write(item + " ");
+}
